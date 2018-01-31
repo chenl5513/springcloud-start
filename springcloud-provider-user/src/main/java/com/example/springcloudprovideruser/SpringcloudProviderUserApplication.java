7a23f0af375875f99fe13c7191c5ba10c1850917
chenl5513 <chenl5513@163.com>
2018-01-31 09:00:44 +0800

    初始化

---
+++ springcloud-provider-user/src/main/java/com/example/springcloudprovideruser/SpringcloudProviderUserApplication.java
@@ -0,0 +1,12 @@
+package com.example.springcloudprovideruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudProviderUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudProviderUserApplication.class, args);
+	}
+}
